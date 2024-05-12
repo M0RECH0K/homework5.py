313000c0cff92e56ed104f281226740235006eee
Dmitry Kharlamov <168891004+M0RECH0K@users.noreply.github.com>
2024-05-12 07:38:04 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -1,7 +1,7 @@
 my_list = ['apple', 'banana', 'orange', 'grapefruit', 'chery', 'peach']
 print(my_list)
 print(my_list[0])
-print(my_list[5])
+print(my_list[-1])
 print(my_list[2:5])
 my_list. remove('grapefruit')
 print(my_list)
@@ -9,6 +9,6 @@ print(my_list)
 
 my_dict = {'one': 1, 'three': 3, 'ten': 10, 'twenty-five': 25}
 print(my_dict)
-my_dict['one']
+print(my_dict['one'])
 my_dict.update({'two': 2})
 print(my_dict)
